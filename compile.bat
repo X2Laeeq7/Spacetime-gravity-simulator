fc44a86d6d99dfb86aa8e07fd5b25f99ce3adcb6
Laeeq <laeeqgeduld87@gmail.com>
2026-09-08 21:14:28 +0200

    Add compile.bat build script

--- compile.bat
+++ compile.bat
@@ -2,8 +2,7 @@
 echo Compiling Gravity Sheet...
 g++ src/main.cpp -o gravity_sheet.exe -I C:\raylib\include -L C:\raylib\lib -lraylib -lopengl32 -lgdi32 -lwinmm
 if %errorlevel% == 0 (
-    echo Success! Running program...
-    gravity_sheet.exe
+    echo Success! 
 ) else (
     echo Compilation failed!
     pause
